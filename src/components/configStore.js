2dccfc14b7d7abf86ff041c3f1f6ace0f8715f87
John Doe <johndoe@example.com>
2024-01-30 14:08:23 +0100

    erreure

--- src/components/configStore.js
+++ src/components/configStore.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import gameReducer from './Reducers/gameReducer';
+import gameReducer from './Reducers/gameReduce';
 
 const store = configureStore({
   reducer: {
