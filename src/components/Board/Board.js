589c0b597ad985e5b8927b90e02a2962e55da21b
John Doe <johndoe@example.com>
2024-02-16 12:56:35 +0100

    dragdrop

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,26 +3,22 @@ import './Board.css';
 import BoardSlot from '../BoardSlot/BoardSlot';
 import { useDrop } from 'react-dnd';
 
-const [{ canDrop, isOver }, dropRef] = useDrop(() => ({
-  accept: 'card',
-  drop: (item, monitor) => {
-    // Logique pour gérer une carte déposée
-    console.log("Card dropped", item);
-  },
-  collect: (monitor) => ({
-    isOver: !!monitor.isOver(),
-    canDrop: !!monitor.canDrop(),
-  }),
-}));
-
-// Attachez `dropRef` à votre élément de plateau ou d'emplacement :
-<div ref={dropRef} className="board-slot">
-  {/* Emplacement pour une carte */}
-</div>
-
 function Board({ slots, onCardDrop }) {
+  const [{ canDrop, isOver }, dropRef] = useDrop(() => ({
+    accept: 'card',
+    drop: (item, monitor) => {
+      // Logique pour gérer une carte déposée
+      console.log("Card dropped", item);
+      if (onCardDrop) onCardDrop(item);
+    },
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+      canDrop: !!monitor.canDrop(),
+    }),
+  }));
+
   return (
-    <div className="board">
+    <div ref={dropRef} className="board">
       {slots.map((slot, index) => (
         <BoardSlot key={index} onDrop={onCardDrop} />
       ))}
@@ -31,5 +27,3 @@ function Board({ slots, onCardDrop }) {
 }
 
 export default Board;
-
-
